0e838eadfb541d172e3dd799a497132571175d0d
Andrew Nguyen Vo <awnguy01@louisville.edu>
2019-09-24 00:45:33 -0400

    Removed unused imports on login component

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,17 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
-import { LocalKeyStore } from 'src/app/classes/constants/local-key-store';
-import { Router } from '@angular/router';
-import {
-  AuthService,
-  GoogleLoginProvider,
-  SocialUser
-} from 'angularx-social-login';
-import { UserService } from 'src/app/services/user.service';
-import { switchMap, catchError, tap } from 'rxjs/operators';
-import { asap } from 'rxjs/internal/scheduler/asap';
-import { from, scheduled } from 'rxjs';
-import { AppAuthService } from 'src/app/services/app-auth.service';
 
 @Component({
   selector: 'app-login',
@@ -19,11 +6,7 @@ import { AppAuthService } from 'src/app/services/app-auth.service';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
-  constructor(
-  ) {}
-
-
+  constructor() {}
 
   ngOnInit() {}
 }
