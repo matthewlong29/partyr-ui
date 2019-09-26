c4dec78bdec1467bfa39e72f55eeb85cbc277542
Andrew Nguyen Vo <awnguy01@louisville.edu>
2019-09-25 20:11:35 -0400

    Changing to router on server.js

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const serverless = require('serverless-http');
 
-var app = express();
+const app = express();
+const router = express.Router();
+
 app.use(cookieParser());
 app.use(bodyParser.json());
 
@@ -27,35 +29,38 @@ app.set('port', process.env.PORT || 3000);
 app.use(express.static(path.join(__dirname, 'public')));
 
 /** COOKIES */
-app.get('/logged-in', (req, res) => {
+router.get('/logged-in', (req, res) => {
   const cookie = req.cookies['LOGGED_IN'];
   const loggedIn = (cookie || '').toUpperCase() === 'TRUE';
   res.send(loggedIn);
 });
 
-app.get('/auth-id-token', (req, res) => {
+router.get('/auth-id-token', (req, res) => {
   const idToken = req.cookies['AUTH_ID_TOKEN'];
   res.send(idToken);
 });
 
-app.delete('/logged-in', (req, res) => {
+router.delete('/logged-in', (req, res) => {
   res.clearCookie('LOGGED_IN').send();
 });
 
-app.delete('/auth-id-token', (req, res) => {
+router.delete('/auth-id-token', (req, res) => {
   res.clearCookie('AUTH_ID_TOKEN').send();
 });
 
-app.get('*', (req, res) => {
+router.get('*', (req, res) => {
   res.sendFile('public/index.html', {
     root: __dirname
   });
 });
+
+app.use('/.netlify/functions/api', router);
 
 if (env && env === 'local') {
   app.listen(app.get('port'), () => {
     console.log('App running on port', app.get('port'));
   });
 } else {
+  module.exports = app;
   module.exports.handler = serverless(app);
 }
