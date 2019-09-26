85c3162a2e55dcd3f649399495734e436c8a889f
Andrew Nguyen Vo <awnguy01@louisville.edu>
2019-09-25 20:24:41 -0400

    Catch errors added

--- src/app/login-guard.ts
+++ src/app/login-guard.ts
@@ -1,10 +1,11 @@
 import { CanActivate } from '@angular/router/src/utils/preactivation';
 import { Router, ActivatedRouteSnapshot, UrlTree } from '@angular/router';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Observable, scheduled } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
 import { AppAuthService } from './services/app-auth.service';
 import { URLStore } from './classes/url-store';
+import { asap } from 'rxjs/internal/scheduler/asap';
 
 @Injectable()
 export class LoginGuard implements CanActivate {
@@ -14,13 +15,15 @@ export class LoginGuard implements CanActivate {
   constructor(readonly router: Router, readonly appAuthSvc: AppAuthService) {}
 
   canActivate(): Observable<boolean | UrlTree> {
-    return this.appAuthSvc
-      .getIsLoggedIn()
-      .pipe(
-        map(
-          (loggedIn: boolean) =>
-            !loggedIn || this.router.parseUrl(URLStore.HOME_URL)
-        )
-      );
+    return this.appAuthSvc.getIsLoggedIn().pipe(
+      map(
+        (loggedIn: boolean) =>
+          !loggedIn || this.router.parseUrl(URLStore.HOME_URL)
+      ),
+      catchError((err: any) => {
+        console.error(err);
+        return scheduled([this.router.parseUrl(URLStore.HOME_URL)], asap);
+      })
+    );
   }
 }
