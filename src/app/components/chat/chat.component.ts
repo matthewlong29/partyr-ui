f74ea5afe90bada18dab9df6fd0d7dab83464cc8
Andrew Nguyen Vo <awnguy01@louisville.edu>
2019-10-27 04:54:53 -0400

    Updated chat component design and functionality

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -4,7 +4,8 @@ import {
   ElementRef,
   Input,
   OnInit,
-  ViewChild
+  ViewChild,
+  HostBinding
 } from '@angular/core';
 import { AuthService, SocialUser } from 'angularx-social-login';
 import { Message } from 'src/app/classes/Message';
@@ -12,7 +13,9 @@ import { PartyrUser } from 'src/app/classes/PartyrUser';
 import { UserService } from 'src/app/services/user.service';
 import * as Stomp from 'stompjs';
 import { ChatService } from 'src/app/services/chat.service';
-import { switchMap } from 'rxjs/operators';
+import { switchMap, finalize, tap, first } from 'rxjs/operators';
+import { FormControl } from '@angular/forms';
+import { concat, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-chat',
@@ -20,12 +23,16 @@ import { switchMap } from 'rxjs/operators';
   styleUrls: ['./chat.component.scss']
 })
 export class ChatComponent implements OnInit, AfterViewChecked {
+  @HostBinding('style.height') hostHeight = '100%';
+  @HostBinding('style.display') hostDisplay = 'flex';
+
   @ViewChild('chatWindow') private chatWindow: ElementRef;
   @Input() chatPurposeName: string;
 
+  newMsgCtrl = new FormControl('');
+
   messages: Array<Message> = [];
   message: Message = new Message();
-  private stompClient: Stomp;
   private partyrUser: PartyrUser = new PartyrUser();
 
   /**
@@ -41,31 +48,43 @@ export class ChatComponent implements OnInit, AfterViewChecked {
    * ngOnInit.
    */
   ngOnInit() {
-    this.authService.authState
-      .pipe(
-        switchMap((currentUser: SocialUser) =>
-          this.userService.getPartyrUserByEmail(currentUser.email)
-        )
-      )
-      .subscribe((partyrUser: PartyrUser) => {
-        this.partyrUser = partyrUser;
-        this.initializeNewMessage();
-      });
-    this.chatService
-      .getAllChat()
-      .subscribe((messages: Message[]) => (this.messages = messages));
-    this.chatService.connectToChat().subscribe((msg: Message) => {
-      this.messages.push(msg);
-    });
+    concat(
+      this.getCurrentUser(),
+      this.getChatMessages(),
+      this.connectToChat()
+    ).subscribe();
+  }
+
+  /**
+   * getCurrentUser
+   */
+  getCurrentUser(): Observable<any> {
+    return this.authService.authState.pipe(
+      switchMap((currentUser: SocialUser) =>
+        this.userService.getPartyrUserByEmail(currentUser.email)
+      ),
+      tap((user: PartyrUser) => (this.partyrUser = user)),
+      first()
+    );
   }
 
   /**
    * getChatMessages
    */
-  public getChatMessages(): void {
-    this.chatService.getAllChat().subscribe(messages => {
-      this.messages = messages;
-    });
+  getChatMessages(): Observable<any> {
+    return this.chatService.getAllChat().pipe(
+      tap((msgs: Message[]) => (this.messages = msgs)),
+      first()
+    );
+  }
+
+  /**
+   * connectToChat
+   */
+  connectToChat(): Observable<any> {
+    return this.chatService
+      .connectToChat()
+      .pipe(tap((msg: Message) => this.messages.push(msg)));
   }
 
   /**
@@ -76,22 +95,24 @@ export class ChatComponent implements OnInit, AfterViewChecked {
   }
 
   /**
-   * initializeMessage.
+   * getTypedMessage.
    */
-  private initializeNewMessage() {
-    this.message = new Message();
-    this.message.author = this.partyrUser.email;
+  private getTypedMessage(): Message {
+    return {
+      author: this.partyrUser.email,
+      content: this.newMsgCtrl.value,
+      timeOfMessage: new Date().toISOString()
+    };
   }
 
   /**
    * sendMessage.
    */
-  sendMessage(event) {
-    if (event.ctrlKey && event.key === 'Enter') {
-      this.message.content += '\n';
-    } else if (event.key === 'Enter') {
-      this.chatService.sendToChat(this.message);
-      this.initializeNewMessage();
+  sendMessage(event: KeyboardEvent) {
+    if (event.key === 'Enter') {
+      const msg: Message = this.getTypedMessage();
+      this.chatService.sendToChat(msg);
+      this.newMsgCtrl.reset('');
     }
   }
 
